1a0a44a54902ccaad501ed3003b01f43656f5712
Vitor Zamprogno Amancio Pereira <vitor.zapereira@gmail.com>
2021-12-14 15:54:36 -0300

    configuração do ambiente

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+	dialect:  ‘postgres’,
+	host: ‘localhost’,
+username:  ‘postgres’,
+	password:  ‘docker’,
+	database: ‘Blog’,
+	define: {
+	timestamp: true,
+	underscored: true,
+	underscoredAll: true,
+},	
+}
